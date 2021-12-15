298c4ef47e7c12335256a0ce0a5bd01d4157d5de
houguang <houguang@sina.cn>
2021-12-16 00:30:39 +0800

    Table组件fixed属性bug演示

--- src/index.tsx
+++ src/index.tsx
@@ -1,89 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { ConfigProvider } from '@arco-design/web-react';
-import zhCN from '@arco-design/web-react/es/locale/zh-CN';
-import enUS from '@arco-design/web-react/es/locale/en-US';
+import { Table } from '@arco-design/web-react';
 import ReactDOM from 'react-dom';
-import { Router, Switch, Route } from 'react-router-dom';
-import axios from 'axios';
-import rootReducer from './redux';
-import history from './history';
-import PageLayout from './layout/page-layout';
-import Setting from './components/Settings';
-import { GlobalContext } from './context';
-import './style/index.less';
-import './mock';
-import Login from './pages/login';
-import checkLogin from './utils/checkLogin';
-
-const store = createStore(rootReducer);
+import React from 'react';
 
 function Index() {
-  const localeName = localStorage.getItem('arco-lang') || 'zh-CN';
-
-  if (!localStorage.getItem('arco-lang')) {
-    localStorage.setItem('arco-lang', localeName);
-  }
-
-  const [locale, setLocale] = useState();
-
-  async function fetchLocale(ln?: string) {
-    const locale = (await import(`./locale/${ln || localeName}`)).default;
-    setLocale(locale);
-  }
-
-  function getArcoLocale() {
-    switch (localeName) {
-      case 'zh-CN':
-        return zhCN;
-      case 'en-US':
-        return enUS;
-      default:
-        return zhCN;
-    }
-  }
-
-  function fetchUserInfo() {
-    axios.get('/api/user/userInfo').then((res) => {
-      store.dispatch({
-        type: 'update-userInfo',
-        payload: { userInfo: res.data },
-      });
-    });
-  }
-
-  useEffect(() => {
-    fetchLocale();
-  }, []);
-
-  useEffect(() => {
-    if (checkLogin()) {
-      fetchUserInfo();
-    } else {
-      history.push('/user/login');
-    }
-  }, []);
-
-  const contextValue = {
-    locale,
-  };
-
-  return locale ? (
-    <Router history={history}>
-      <ConfigProvider locale={getArcoLocale()}>
-        <Provider store={store}>
-          <GlobalContext.Provider value={contextValue}>
-            <Switch>
-              <Route path="/user/login" component={Login} />
-              <Route path="/" component={PageLayout} />
-            </Switch>
-            <Setting />
-          </GlobalContext.Provider>
-        </Provider>
-      </ConfigProvider>
-    </Router>
-  ) : null;
+  const columns = [
+    { title: 'A', width: 300, key: 'a', dataIndex: 'a', fixed: 'left' },
+    { title: 'B', width: 300, key: 'b', dataIndex: 'b' },
+    { title: 'C', width: 300, key: 'c', dataIndex: 'c' },
+    { title: 'D', width: 300, key: 'd', dataIndex: 'd' },
+    { title: 'E', width: 300, key: 'e', dataIndex: 'e' },
+    { title: 'F', width: 300, key: 'f', dataIndex: 'f', fixed: 'right' },
+  ];
+  const data = [
+    { a: 1, b: 1, c: 1, d: 1, e: 1, f: 1 },
+    { a: 2, b: 2, c: 2, d: 2, e: 2, f: 2 },
+    { a: 3, b: 3, c: 3, d: 3, e: 3, f: 3 },
+  ];
+  return (
+    <div>
+      <Table rowKey="a" columns={columns} data={data} scroll={{ x: true }} />
+    </div>
+  );
 }
 
 ReactDOM.render(<Index />, document.getElementById('root'));
